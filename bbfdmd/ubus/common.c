2976fdda4a509fca53c3c5007914774b43b3971d
Amin Ben Romdhane <amin.benromdhane@iopsys.eu>
2025-03-04 09:19:09 +0000

    Increase ubus timeout to 5s for sync calls

--- bbfdmd/ubus/common.c
+++ bbfdmd/ubus/common.c
@@ -190,7 +190,7 @@ void run_sync_call(const char *ubus_obj, const char *ubus_method, struct blob_at
 		BBFDM_FREE(json_str);		
 	}
 
-	BBFDM_UBUS_INVOKE_SYNC(ubus_obj, ubus_method, req_buf.head, 2000, sync_callback, bb_response);
+	BBFDM_UBUS_INVOKE_SYNC(ubus_obj, ubus_method, req_buf.head, 5000, sync_callback, bb_response);
 
 	blob_buf_free(&req_buf);
 }
